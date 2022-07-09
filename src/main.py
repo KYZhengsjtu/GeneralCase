24743d0eb10ccdbb32a0544226e7fad19cfb6064
郑开元 <kaiyuan.zheng@sh-smartstate.com>
2022-07-10 01:47:28 +0800

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+a1 = [8, 16]
+a2 = [16, 24]
+a3 = [0, 8]
+a = [a1, a2, a3]
+if __name__ == '__main__':
+    for i in range(3):
+        if a[0][0] == a[i][1]:
+            if i > 1:
+
+                print("")
